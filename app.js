00b3517c3d15c983d4794380e6a9187de88dd805
Dip Ghosh <dipnsucse@gmail.com>
2026-02-04 23:52:13 +0600

    backend implementation using node

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer((request, response) => {
-    response.setHeader('Content-Type', 'text/html');
-    response.write('<h1>Hello there !</h1>');
-    response.end();
+const app = express();
+
+//middleware to parse URL encoded data
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//middleware to set response header for html
+app.use((req,res,next)=>{
+    res.setHeader('Content-Type','text/html');
+    next();
+})
+
+//main route to handle POST request
+app.use((req,res)=>{
+    const body = req.body || {};
+    let userName = body.username;
+
+    if (!userName) userName = 'Unknown';
+
+    res.send(`
+            <h1>${userName}</h1>
+             <form method="POST" action="/">
+                <input type="text" name="username">
+                <button type="submit"> Send</button>
+             </form>
+        `);
+})
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
 });
-
-server.listen(3000);
